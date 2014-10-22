f111a986d191f2fcbb0baec0600e43cc255bf17f
daynaclowe <daynalowe@Dayna-Lowes-MacBook.local>
2014-10-22 15:12:02 -0400

    checking the variable scope

--- exercise5_methods.rb
+++ exercise5_methods.rb
@@ -1,7 +1,21 @@
-def method_name
-		"The interperter reads over me but does nothing"
-		return 25
-		"The interperter does not read me because the return keyword above forces the interperter to exit the method"
+# IMPLICIT vs EXPLICIT RETURN
+#def method_name
+# 		"The interperter reads over me but does nothing"
+# 		return 25
+# 		"The interperter does not read me because the return keyword above forces the interperter to exit the method"
 
+# end
+
+# #PARAMETERS
+# def reverse_sign(argument)
+# 	-1 * argument #Code to be executed
+# end
+
+# reverse_sign(56) #-56  
+# to call on in terminal type: reverse_sign(56) => -56 
+
+#VARIABLE SCOPE
+def new_method(number)
+	number
 end
-
+#to call on in terminal type: new_method(5) => 5
