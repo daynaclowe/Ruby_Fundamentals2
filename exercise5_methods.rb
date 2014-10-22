048c8835c2f30698dd4eca5284d5559b7404c369
daynaclowe <daynalowe@Dayna-Lowes-MacBook.local>
2014-10-22 14:50:49 -0400

    some message

---
+++ exercise5_methods.rb
@@ -0,0 +1,7 @@
+def method_name
+		"The interperter reads over me but does nothing"
+		return 25
+		"The interperter does not read me because the return keyword above forces the interperter to exit the method"
+
+end
+
