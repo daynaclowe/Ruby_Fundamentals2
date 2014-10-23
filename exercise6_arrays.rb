0b1b6b2788f14663db7dfa97751aadc5bd7d6825
daynaclowe <daynalowe@Dayna-Lowes-MacBook.local>
2014-10-22 20:55:51 -0400

    Step 1 + remembering the rice.

--- exercise6_arrays.rb
+++ exercise6_arrays.rb
@@ -1,1 +1,25 @@
 grocery_list = ["arugula", "apples", "peanut butter", "paneer"]
+
+grocery_list << "rice"
+
+#3.2 Iteration RM
+grocery_list.each do |item|	
+	puts "* #{item}"
+end
+
+
+
+
+
+#EXTRAS
+# def likes(x)
+# 	"There are #{x}"
+# end
+# puts likes("dogs")
+
+# def list(grocery)
+# 	puts "*  #{grocery_list}"
+# end
+
+# grocery_list
+
